813c068af0da6379df81d97a47789371a4d7f410
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-06-11 09:17:02 +0200

    Data entities where we can get to them

---
+++ DotNetExercise16/DotNetExercise16/Entities/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetExercise16.Entities
+{
+    public class Machine
+    {
+        public required string Name { get; set; }
+        public required Guid Id { get; set; }
+        public bool IsOnline { get; set; } = true;
+        public List<Message> Messages { get; } = new List<Message> { };
+    }
+}
---
+++ DotNetExercise16/DotNetExercise16/Entities/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetExercise16.Entities
+{
+    public class Message
+    {
+        public required string Content { get; set; }
+        public DateTime Time { get; set; }
+    }
+
+}
