3ab1c03be1f9b6ad95b095a38005eed617f7f707
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-06-12 10:21:42 +0200

    In preparation for a slightly less transient state, the machine park entity

---
+++ DotNetExercise16/DotNetExercise16/Entities/MachinePark.cs
@@ -0,0 +1,27 @@
+﻿namespace DotNetExercise16.Entities
+{
+    public class MachinePark
+    {
+        public List<Machine> Machines {init; get;} = new List<Machine>();
+        public MachinePark()
+        {
+            Machines.Add(new Machine()
+            {
+                Name = "The one by the door",
+                Id = Guid.NewGuid()
+            });
+            Machines.Add(new Machine()
+            {
+                Name = "The one behind the bookshelf",
+                Id = Guid.NewGuid(),
+                IsOnline = false
+            });
+            Machines[1].Messages.Add(
+            new Message()
+            {
+                Time = DateTime.Now,
+                Content = "Turned off anonymously with emergency button"
+            });
+        }
+    }
+}
